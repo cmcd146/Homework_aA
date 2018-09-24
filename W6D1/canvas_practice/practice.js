e4fff1c3a2bfb0e1e3e2f2a0373fd1953f9a0db5
Chris McDonald <mcdonaldchristopherkevin@gmail.com>
2018-09-24 08:55:01 -0700

    Finish W6D1

---
+++ W6D1/canvas_practice/practice.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById("mycanvas");
+  canvas.width = 500;
+  canvas.height = 500;
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'red';
+  ctx.fillRect(0,0,500,500);
+
+  ctx.beginPath();
+  ctx.arc(100,75,50,0,2*Math.PI);
+  ctx.strokeStyle = 'purple';
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.fillStyle = "green";
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.moveTo(75, 50);
+  ctx.lineTo(100, 75);
+  ctx.lineTo(100, 25);
+  ctx.fill();
+});
